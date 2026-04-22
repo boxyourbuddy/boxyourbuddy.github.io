00ad9e8c077c8c9347481eea01a9d4e70d7f5e1c
boxyourbuddy <danstout.4@gmail.com>
2026-04-21 17:43:03 -0700

    Delete TV Stats.app/Contents directory

--- TV Stats.app/Contents/Resources/config.js
+++
@@ -1,4 +0,0 @@
-// ── Trakt API Credentials ────────────────────────────────────────────────────
-// ⚠️  Add this file to .gitignore before pushing to a public repository.
-const TRAKT_CLIENT_ID     = "fc0d1f56b17f9509aef32b81f224e58f43ac01206c8e10a3ac739521e07c8f3a";
-const TRAKT_CLIENT_SECRET = "3a5014ee1eae793395fddec6db65cf151d63a4688d2f51ba3643404b13c5b509";
